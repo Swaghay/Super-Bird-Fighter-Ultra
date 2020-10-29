f4496e9e5a461b3612f875b6d82ac606c46df96e
AnthonyKuismi <61592276+AnthonyKuismi@users.noreply.github.com>
2020-10-29 10:45:52 -0500

    Animation swag xqc leat fingies

--- Chicken Fighter Ultra/Chicken Fighter Ultra/GameScene.swift
+++ Chicken Fighter Ultra/Chicken Fighter Ultra/GameScene.swift
@@ -13,6 +13,7 @@ import UIKit
 
 class GameScene: SKScene {
     var x_direction = ""
+    var isAnimate = false
     var x_max_speed:CGFloat = 400
     var x_acc:CGFloat = 40
     var Right_Arrow = SKSpriteNode()
@@ -21,6 +22,8 @@ class GameScene: SKScene {
     var LeftWall = SKSpriteNode(color: UIColor.red, size: CGSize(width: 100, height: 900))
     var Left_Arrow = SKSpriteNode()
     var Player = SKSpriteNode()
+    let buttonSize = CGSize(width: 96 , height: 96)
+    let CharacterSize = CGSize(width: 96 , height: 96)
    
     override func didMove(to view: SKView) {
 
@@ -28,37 +31,64 @@ class GameScene: SKScene {
     }
     
     func setup(){
+        set_names()
+        set_textures()
+        set_sizes()
+        set_physics()
+        set_positions()
+        addchildren()
+    }
+    
+    func set_names(){
         Platform.name = "Platform"
         RightWall.name = "RightWall"
         LeftWall.name = "LeftWall"
         Player.name = "Player"
         Right_Arrow.name = "Right"
         Left_Arrow.name = "Left"
-        set_filtering_mode(fileNamed: "Chickette_with_skirt", node: Player)
+
+    }
+    
+    func set_textures(){
+        //set_filtering_mode(fileNamed: "Chickette_with_skirt", node: Player)
+        //setTexture(folderName: "GooseWalk", sprite: Player, spriteName: "goose_walk",speed: 15)
+        set_filtering_mode(fileNamed: "goose_stand", node: Player)
         set_filtering_mode(fileNamed: "RightArrow", node: Right_Arrow)
         set_filtering_mode(fileNamed: "LeftArrow", node: Left_Arrow)
-        let buttonSize = CGSize(width: 96 , height: 96)
-        let CharacterSize = CGSize(width: 96 , height: 96)
+    }
+    
+    func set_sizes(){
         Right_Arrow.size = buttonSize
         Player.size = CharacterSize
+        Left_Arrow.size = buttonSize
+    }
+    
+    func set_physics(){
+        RightWall.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 100, height: 900))
+        RightWall.physicsBody?.affectedByGravity = false
+        RightWall.physicsBody?.isDynamic = false
+        
+        LeftWall.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 100, height: 900))
+        LeftWall.physicsBody?.affectedByGravity = false
+        LeftWall.physicsBody?.isDynamic = false
         Player.physicsBody = SKPhysicsBody(rectangleOf: CharacterSize)
         Player.physicsBody?.affectedByGravity = true
         Platform.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 900, height: 100))
         Platform.physicsBody?.affectedByGravity = false
         Platform.physicsBody?.friction = 1
         Platform.physicsBody?.isDynamic = false
+    }
+    
+    func set_positions(){
+        
         Platform.position = CGPoint(x: 0, y: -200)
-        RightWall.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 100, height: 900))
-        RightWall.physicsBody?.affectedByGravity = false
-        RightWall.physicsBody?.isDynamic = false
-        RightWall.position = CGPoint(x: 435, y: 0)
-        LeftWall.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 100, height: 900))
-        LeftWall.physicsBody?.affectedByGravity = false
-        LeftWall.physicsBody?.isDynamic = false
         LeftWall.position = CGPoint(x: -450, y: 0)
-        Left_Arrow.size = buttonSize
         Left_Arrow.position = CGPoint(x: -300, y: -200)
         Right_Arrow.position = CGPoint(x: -200,y: -200)
+        RightWall.position = CGPoint(x: 435, y: 0)
+    }
+    
+    func addchildren(){
         addChild(Platform)
         addChild(Player)
         addChild(Left_Arrow)
@@ -97,11 +127,21 @@ class GameScene: SKScene {
         if x_direction == "right"{
             if (Player.physicsBody?.velocity.dx)! < x_max_speed{
                 Player.physicsBody?.velocity.dx += x_acc
+                if (!isAnimate){
+                    setTexture(folderName: "GooseWalk", sprite: Player, spriteName: "goose_walk",speed: 15)
+                    isAnimate = true
+                }
             }
+            
         }else if x_direction == "left"{
             if (Player.physicsBody?.velocity.dx)! > -x_max_speed{
                 Player.physicsBody?.velocity.dx -= x_acc
+                self.Player.removeAllActions()
+                isAnimate = false
             }
+        }else {
+            Player.removeAllActions()
+            isAnimate = false
         }
         
     }
@@ -114,14 +154,31 @@ class GameScene: SKScene {
                     self.Right_Arrow.alpha = 0.5
                     print("right begin")
                     self.x_direction = "right"
+                    
                 }
             }else  if node.name == "Left"{
                 if (self.Left_Arrow.contains(location)){
                     self.Left_Arrow.alpha = 0.5
                     print("left begin")
                     self.x_direction = "left"
+                    
                 }
             }
         }
     }
+    
+    func setTexture(folderName: String,sprite:SKSpriteNode,spriteName: String,speed:Double){
+           let textureAtlas = SKTextureAtlas(named: folderName)
+           var frames: [SKTexture] = []
+           for i in 0...textureAtlas.textureNames.count - 1{
+               let name = "\(spriteName)\(i).png"
+               let texture = SKTexture(imageNamed: name)
+               
+               texture.filteringMode = SKTextureFilteringMode.nearest
+               frames.append(texture)
+           }
+             
+           let animation = SKAction.animate(with: frames, timePerFrame: 1/speed)
+           sprite.run(SKAction.repeatForever(animation))
+       }
 }
