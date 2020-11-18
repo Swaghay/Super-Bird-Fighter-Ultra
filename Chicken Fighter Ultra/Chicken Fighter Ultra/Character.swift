75afa3bb0348b7be22a51cc332fe3c8064ac0b53
AnthonyKuismi <61592276+AnthonyKuismi@users.noreply.github.com>
2020-11-18 10:50:02 -0600

    added character.json

---
+++ Chicken Fighter Ultra/Chicken Fighter Ultra/Character.swift
@@ -0,0 +1,135 @@
+//
+//  Character.swift
+//  Chicken Fighter Ultra
+//
+//  Created by 90308589 on 11/18/20.
+//
+
+import SpriteKit
+import GameplayKit
+import UIKit
+import Foundation
+
+class Character:SKSpriteNode{
+    
+    var x_direction = ""
+    var isWalk = false
+    var isFly = false
+    
+    var max_jumps: CGFloat = 0
+    var jump_velocity: CGFloat = 0
+    var x_max_speed:CGFloat = 0
+    var x_acc:CGFloat = 0
+    var CharacterSize: CGSize = CGSize.zero
+    
+    var run_animation_sprite_name: String = ""
+    var run_animation_folder_name: String = ""
+    var fly_animation_sprite_name: String = ""
+    var fly_animation_folder_name: String = ""
+    var attack_animation_sprite_name: String = ""
+    var attack_animation_folder_name: String = ""
+    
+    
+    init(texture: SKTexture!){
+        super.init(texture: texture, color: UIColor.clear, size: texture.size())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+   public func setvalues(jumps:CGFloat, jump_vel:CGFloat, max_x_speed:CGFloat, acc:CGFloat, size:CGSize){
+        max_jumps = jumps
+        jump_velocity = jump_vel
+        x_max_speed = max_x_speed
+        x_acc = acc
+        CharacterSize = size
+        self.physicsBody = SKPhysicsBody(rectangleOf: CharacterSize)
+        self.physicsBody?.affectedByGravity = true
+        self.physicsBody?.allowsRotation = false
+        self.physicsBody?.categoryBitMask = 1
+        self.physicsBody?.collisionBitMask = 2
+        self.physicsBody?.contactTestBitMask = 2
+    }
+    
+    public func setAnimations(run_sprite:String,run_folder:String,fly_sprite:String,fly_folder:String,attack_sprite:String,attack_folder:String){
+        run_animation_sprite_name = run_sprite
+        run_animation_folder_name  = run_folder
+        fly_animation_sprite_name = fly_sprite
+        fly_animation_folder_name = fly_folder
+        attack_animation_sprite_name = attack_sprite
+        attack_animation_folder_name = attack_folder
+    }
+    
+    func setTexture(folderName: String,sprite:SKSpriteNode,spriteName: String,speed:Double){
+           let textureAtlas = SKTextureAtlas(named: folderName)
+           var frames: [SKTexture] = []
+           for i in 0...textureAtlas.textureNames.count - 1{
+               let name = "\(spriteName)\(i).png"
+               let texture = SKTexture(imageNamed: name)
+               texture.filteringMode = SKTextureFilteringMode.nearest
+               frames.append(texture)
+           }
+             
+           let animation = SKAction.animate(with: frames, timePerFrame: 1/speed)
+           sprite.run(SKAction.repeatForever(animation))
+    }
+    
+    
+     func addEmiter(loc: CGPoint,file:String){
+         let emitter = SKEmitterNode(fileNamed: file)
+         emitter?.name = "emitter"
+         emitter?.zPosition = 2;
+         emitter?.position = CGPoint(x: loc.x, y: loc.y )
+         addChild(emitter!)
+         
+         emitter?.run(SKAction.sequence([
+             SKAction.wait(forDuration: 0.5),SKAction.removeFromParent()]))
+     }
+
+    public func update_character(){
+        if x_direction != "" && self.position.y < 0{
+            addEmiter(loc: CGPoint(x: self.position.x, y: self.position.y-CharacterSize.height/2), file: "PlayerWalkDust")
+        }
+
+        if x_direction == "right" && !isFly{
+            if (self.physicsBody?.velocity.dx)! < x_max_speed{
+                self.physicsBody?.velocity.dx += x_acc
+                if (!isWalk){
+                    self.removeAllActions()
+                    setTexture(folderName: "gooseMove", sprite: self, spriteName: "goose_walk",speed: 100)
+                    self.xScale = 1
+                    isWalk = true
+                }
+            }
+        }else if x_direction == "left" && !isFly{
+            if (self.physicsBody?.velocity.dx)! > -x_max_speed{
+                self.physicsBody?.velocity.dx -= x_acc
+                if (!isWalk){
+                    self.removeAllActions()
+                    setTexture(folderName: "gooseMove", sprite: self, spriteName: "goose_walk",speed: 100)
+                    self.xScale = -1
+                    isWalk = true
+                }
+            }
+        }else if (self.physicsBody?.velocity.dy)! >= 5 && !isWalk{
+            if x_direction == "left"{
+                self.xScale = -1
+            }else{
+                self.xScale = 1
+            }
+            if (!isFly){
+                self.removeAllActions()
+                setTexture(folderName: "goose_flying_good", sprite: self, spriteName: "goose_flying",speed: 15)
+                isWalk = false
+                isFly = true
+            }
+            
+        }else {
+            self.removeAllActions()
+            isWalk = false
+            isFly = false
+        }
+        
+    }
+}
--- Chicken Fighter Ultra/Chicken Fighter Ultra/GameScene.swift
+++ Chicken Fighter Ultra/Chicken Fighter Ultra/GameScene.swift
@@ -11,33 +11,28 @@ import UIKit
 
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    var x_direction = ""
-    var isWalk = false
-    var isFly = false
     var current_jumps = 0
     var max_jumps = 2
     var backround = SKSpriteNode()
-    var jump_velocity = 250
-    var x_max_speed:CGFloat = 400
-    var x_acc:CGFloat = 40
+    var dummy = SKSpriteNode(color: UIColor.red, size: CGSize(width: 50, height: 50))
+    var Punch_button = SKSpriteNode()
     var Right_Arrow = SKSpriteNode()
     var Platform = SKSpriteNode(color: UIColor.clear, size: CGSize(width: 900, height: 100))
     var RightWall = SKSpriteNode(color: UIColor.red, size: CGSize(width: 100, height: 900))
     var LeftWall = SKSpriteNode(color: UIColor.red, size: CGSize(width: 100, height: 900))
     var jump_button = SKSpriteNode()
     var Left_Arrow = SKSpriteNode()
-    var Player = SKSpriteNode()
+    var Player = Character(texture: SKTexture(imageNamed: "Chickette"))
     let buttonSize = CGSize(width: 96 , height: 96)
     let CharacterSize = CGSize(width: 96 , height: 96)
     var Play_Button = SKSpriteNode()
     
     override func didMove(to view: SKView) {
-
         setup()
     }
     
     func setup(){
-        
+        Player.setvalues(jumps: 2, jump_vel: 300, max_x_speed: 200, acc: 200, size: CharacterSize)
         set_names()
         set_textures()
         set_sizes()
@@ -55,17 +50,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         Left_Arrow.name = "Left"
         Play_Button.name = "PlayButton"
         jump_button.name = "JumpButton"
+        Punch_button.name = "PunchButton"
+        dummy.name = "dummy"
     }
     
     func set_textures(){
-        //set_filtering_mode(fileNamed: "Chickette_with_skirt", node: Player)
-        //setTexture(folderName: "GooseWalk", sprite: Player, spriteName: "goose_walk",speed: 15)
-        set_filtering_mode(fileNamed: "goose_stand", node: Player)
         set_filtering_mode(fileNamed: "RightArrow", node: Right_Arrow)
         set_filtering_mode(fileNamed: "LeftArrow", node: Left_Arrow)
         set_filtering_mode(fileNamed: "StartButton", node: Play_Button)
         set_filtering_mode(fileNamed: "JumpButton", node: jump_button)
         set_filtering_mode(fileNamed: "farmyard", node: backround)
+        set_filtering_mode(fileNamed: "Punch Button", node: Punch_button)
+        set_filtering_mode(fileNamed: "dummy", node: dummy)
     }
     
     func set_sizes(){
@@ -74,6 +70,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         Left_Arrow.size = buttonSize
         Play_Button.size = buttonSize
         jump_button.size = buttonSize
+        Punch_button.size = buttonSize
+        dummy.size = CharacterSize
         backround.size = CGSize(width: 1000, height: 800)
     }
     
@@ -90,21 +88,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         LeftWall.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 100, height: 900))
         LeftWall.physicsBody?.affectedByGravity = false
         LeftWall.physicsBody?.isDynamic = false
-        Player.physicsBody = SKPhysicsBody(rectangleOf: CharacterSize)
-        Player.physicsBody?.affectedByGravity = true
-        Player.physicsBody?.allowsRotation = false
+       
         Platform.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 900, height: 100))
         Platform.physicsBody?.affectedByGravity = false
         Platform.physicsBody?.friction = 1
         Platform.physicsBody?.isDynamic = false
         Platform.physicsBody?.categoryBitMask = 2
         Platform.physicsBody?.contactTestBitMask = 1
-        Player.physicsBody?.categoryBitMask = 1
-        Player.physicsBody?.collisionBitMask = 2
-        Player.physicsBody?.contactTestBitMask = 2
+        
         
         LeftWall.physicsBody?.categoryBitMask = 2
         RightWall.physicsBody?.categoryBitMask = 2
+        
+        dummy.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
     }
     
     func set_positions(){
@@ -115,6 +111,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         Right_Arrow.position = CGPoint(x: -200,y: -200)
         RightWall.position = CGPoint(x: 435, y: 0)
         jump_button.position = CGPoint(x: 200, y: -200)
+        Punch_button.position = CGPoint(x: 300, y: -200)
         backround.zPosition = -1
     }
     
@@ -128,6 +125,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(LeftWall)
         addChild(jump_button)
         addChild(backround)
+        addChild(Punch_button)
+        addChild(dummy)
     }
     
     
@@ -151,12 +150,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let location = touches.first!.location(in: self)
         if Right_Arrow.contains(location){
             print("right end")
-           x_direction = ""
+            Player.x_direction = ""
             Right_Arrow.alpha = 1
         }
         if Left_Arrow.contains(location){
             print("left end")
-            x_direction = ""
+            Player.x_direction = ""
             Left_Arrow.alpha = 1
 
         }
@@ -165,54 +164,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             jump_button.alpha = 1
 
         }
+        if Punch_button.contains(location){
+            print("punch end")
+            Punch_button.alpha = 1
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
-        if x_direction != "" && Player.position.y < 0{
-            addEmiter(loc: CGPoint(x: Player.position.x, y: Player.position.y-CharacterSize.height/2), file: "PlayerWalkDust")
-        }
-
-        if x_direction == "right" && !isFly{
-            if (Player.physicsBody?.velocity.dx)! < x_max_speed{
-                Player.physicsBody?.velocity.dx += x_acc
-                if (!isWalk){
-                    self.Player.removeAllActions()
-                    setTexture(folderName: "gooseMove", sprite: Player, spriteName: "goose_walk",speed: 100)
-                    Player.xScale = 1
-                    isWalk = true
-                }
-            }
-        }else if x_direction == "left" && !isFly{
-            if (Player.physicsBody?.velocity.dx)! > -x_max_speed{
-                Player.physicsBody?.velocity.dx -= x_acc
-                if (!isWalk){
-                    self.Player.removeAllActions()
-                    setTexture(folderName: "gooseMove", sprite: Player, spriteName: "goose_walk",speed: 100)
-                    Player.xScale = -1
-                    isWalk = true
-                }
-            }
-        }else if (Player.physicsBody?.velocity.dy)! >= 5 && !isWalk{
-            if x_direction == "left"{
-                Player.xScale = -1
-                print("Abhay left")
-            }else{
-                Player.xScale = 1
-                print("Abhay right")
-            }
-            if (!isFly){
-                self.Player.removeAllActions()
-                setTexture(folderName: "gooseflying", sprite: Player, spriteName: "goose_flying",speed: 15)
-                isWalk = false
-                isFly = true
-            }
-            
-        }else {
-            Player.removeAllActions()
-            isWalk = false
-            isFly = false
-        }
-        
+        Player.update_character()
     }
     
     func buttonPress(touch: UITouch){
@@ -222,16 +181,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 if (self.Right_Arrow.contains(location)){
                     self.Right_Arrow.alpha = 0.5
                     print("right begin")
-                    self.x_direction = "right"
-                    self.isFly = false
+                    Player.x_direction = "right"
+                    Player.isFly = false
                     
                 }
             }else  if node.name == "Left"{
                 if (self.Left_Arrow.contains(location)){
                     self.Left_Arrow.alpha = 0.5
                     print("left begin")
-                    self.x_direction = "left"
-                    self.isFly = false
+                    Player.x_direction = "left"
+                    Player.isFly = false
                     
                 }
             }else if node.name == "PlayButton"{
@@ -245,28 +204,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                     print("jump")
                     if self.current_jumps < self.max_jumps{
                         self.current_jumps += 1
-                        self.Player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: self.jump_velocity))
-                        self.isWalk = false
+                        self.Player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: Player.jump_velocity))
+                        Player.isWalk = false
                     }
+                }
+            }else if node.name == "PunchButton"{
+                if self.Punch_button.contains(location){
+                    self.Punch_button.alpha = 0.5
+                    print("punch")
                 }
             }
         }
     }
     
-    func setTexture(folderName: String,sprite:SKSpriteNode,spriteName: String,speed:Double){
-           let textureAtlas = SKTextureAtlas(named: folderName)
-           var frames: [SKTexture] = []
-           for i in 0...textureAtlas.textureNames.count - 1{
-               let name = "\(spriteName)\(i).png"
-               let texture = SKTexture(imageNamed: name)
-               texture.filteringMode = SKTextureFilteringMode.nearest
-               frames.append(texture)
-           }
-             
-           let animation = SKAction.animate(with: frames, timePerFrame: 1/speed)
-           sprite.run(SKAction.repeatForever(animation))
-       }
-    
+   
     func addEmiter(loc: CGPoint,file:String){
         let emitter = SKEmitterNode(fileNamed: file)
         emitter?.name = "emitter"
@@ -283,10 +234,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let nodeB = contact.bodyB.node
         if nodeA!.name == "Player" && nodeB!.name == "Platform"{
             current_jumps = 0
-            isFly = false
+            Player.isFly = false
         } else if nodeB!.name == "Player" && nodeA!.name == "Platform"{
             current_jumps = 0
-            isFly = false
+            Player.isFly = false
         }
     }
 }
